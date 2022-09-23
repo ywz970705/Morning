ec5fbc3e3dd54833264a6cb50d339ef99683dea7
ywz970705 <114229268+ywz970705@users.noreply.github.com>
2022-09-23 19:20:03 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -79,19 +79,19 @@ export const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {"type": "*生日", "name": "鱼宝", "year": "1997", "date": "07-26"},
-        //{"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
+        {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {"type": "生日", "name": "小严", "year": "1997", "date": "07-05"},
-        //{"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+        {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
         {"keyword": "love_day", date: "2016-11-11"},
         // 结婚纪念日
-        //{"keyword": "marry_day", date: "2022-09-09"},
+        {"keyword": "marry_day", date: "2022-09-09"},
         // 退伍日
-        //{"keyword": "ex_day", date: "2022-09-10"},
+        {"keyword": "ex_day", date: "2022-09-10"},
       ]
     },
     {
